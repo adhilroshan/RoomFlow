f3ad201217ff611c32af83d6a0a286cbe40467a5
Sharoon <sharoonrafeek@gmail.com>
2023-05-05 13:48:17 +0530

    spaceview implemented.

--- frontend/src/components/Space/CreateSpaceView.js
+++ frontend/src/components/Space/CreateSpaceView.js
@@ -1,36 +1,38 @@
+import { useForm } from 'react-hook-form';
+
 export default function CreateSpaceView() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
     return <>
         <div className="max-w-2xl mx-auto bg-white p-16">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="grid gap-6 mb-6 lg:grid-cols-2">
                     <div>
                         <label
                             htmlFor="first_name"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                         >
-
+                            Catagory
                         </label>
-                        <input
-                            type="text"
-                            id="first_name"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="John"
-                            required=""
-                        />
+                        <select {...register("Catogory", { required: true })} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+                            <option value="Room">Room</option>
+                            <option value="Parking">Parking</option>
+                            <option value="Storage">Storage</option>
+                        </select>
                     </div>
                     <div>
                         <label
                             htmlFor="last_name"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                         >
-                            Last name
+                            Price
                         </label>
                         <input
-                            type="text"
+                            type="number"
                             id="last_name"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Doe"
-                            required=""
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" s
+                            {...register("Price", { required: true })}
                         />
                     </div>
                     <div>
@@ -38,14 +40,13 @@ export default function CreateSpaceView() {
                             htmlFor="company"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                         >
-                            Company
+                            Area of the space
                         </label>
                         <input
-                            type="text"
+                            type="number"
                             id="company"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Flowbite"
-                            required=""
+                            {...register("Area", { required: true })}
                         />
                     </div>
                     <div>
@@ -56,44 +57,31 @@ export default function CreateSpaceView() {
                             Phone number
                         </label>
                         <input
-                            type="tel"
+                            type="number"
                             id="phone"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="123-45-678"
                             pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                            required=""
+                            {...register("PhoneNumber", { required: true })}
+
                         />
                     </div>
                     <div>
                         <label
-                            htmlFor="website"
+                            htmlFor="location"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                         >
-                            Website URL
+                            Location
                         </label>
                         <input
                             type="url"
                             id="website"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="flowbite.com"
-                            required=""
+                            {...register("Location", { required: true })}
+
                         />
                     </div>
-                    <div>
-                        <label
-                            htmlFor="visitors"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >
-                            Unique visitors (per month)
-                        </label>
-                        <input
-                            type="number"
-                            id="visitors"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder=""
-                            required=""
-                        />
-                    </div>
+
                 </div>
                 <div className="mb-6">
                     <label
@@ -106,64 +94,13 @@ export default function CreateSpaceView() {
                         type="email"
                         id="email"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="john.doe@company.com"
-                        required=""
+                        placeholder="example@email.com"
+                        {...register("Email", { required: true })}
+
                     />
                 </div>
-                <div className="mb-6">
-                    <label
-                        htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="•••••••••"
-                        required=""
-                    />
-                </div>
-                <div className="mb-6">
-                    <label
-                        htmlFor="confirm_password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Confirm password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirm_password"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="•••••••••"
-                        required=""
-                    />
-                </div>
-                <div className="flex items-start mb-6">
-                    <div className="flex items-center h-5">
-                        <input
-                            id="remember"
-                            type="checkbox"
-                            defaultValue=""
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                            required=""
-                        />
-                    </div>
-                    <label
-                        htmlFor="remember"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                    >
-                        I agree with the{" "}
-                        <a
-                            href="#"
-                            className="text-blue-600 hover:underline dark:text-blue-500"
-                        >
-                            terms and conditions
-                        </a>
-                        .
-                    </label>
-                </div>
+
+
                 <button
                     type="submit"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -171,18 +108,6 @@ export default function CreateSpaceView() {
                     Submit
                 </button>
             </form>
-            <p className="mt-5">
-                These input field components is part of a larger, open-source library of
-                Tailwind CSS components. Learn more by going to the official{" "}
-                <a
-                    className="text-blue-600 hover:underline"
-                    href="https://flowbite.com/docs/getting-started/introduction/"
-                    target="_blank"
-                >
-                    Flowbite Documentation
-                </a>
-                .
-            </p>
         </div>
 
     </>
